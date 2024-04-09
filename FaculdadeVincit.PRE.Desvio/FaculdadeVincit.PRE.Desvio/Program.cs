b3f6d0aedf0abd36f0ad3cbee91cf584806efe9a
Nanda  Warttmann <nandawarttmann@gmail.com>
2024-04-09 13:31:16 +0100

    new activities upload
    new activities about diversion and loops

---
+++ FaculdadeVincit.PRE.Desvio/FaculdadeVincit.PRE.Desvio/Program.cs
@@ -0,0 +1,15 @@
+﻿//ESTRUTURA DE DESVIO
+
+//Tenho dinheiro para um sorvete?
+float salario;
+
+System.Console.WriteLine("Quanto você ganha?");
+salario = float.Parse(Console.ReadLine());
+
+//expressão verdade: booleana
+if (salario < 14.90f) {
+    System.Console.WriteLine("Você não tem dinheiro para o sorvete");
+} else {
+    System.Console.WriteLine("Baunilha, Chocolate ou Misto?");
+}
+Console.ReadKey();
---
+++ FaculdadeVincit.PRE.LaçosdeRepetição/FaculdadeVincit.PRE.LaçosdeRepetição/Program.cs
@@ -0,0 +1,49 @@
+﻿//LAÇOS DE REPETIÇÃO
+//Uso do for
+
+/*int valor;
+for (int valor = 1; valor <= 10; valor++) {
+    Console.WriteLine(valor);
+}*/
+
+
+
+
+//Tabuada do 7
+/*int valor;
+
+for (valor = 1; valor <= 10; valor ++) {
+    System.Console.WriteLine(valor * 7);
+}
+Console.ReadKey();*/
+
+
+
+
+//Tabuada definida pelo usuário
+/*int valor;
+int valor_informado;
+
+System.Console.WriteLine("Informe o múltiplo: ");
+valor_informado = int.Parse(Console.ReadLine());
+
+for (valor = 1; valor <=  10; valor++) {
+    System.Console.WriteLine(valor * valor_informado);
+}
+Console.ReadKey();*/
+
+
+
+
+//Todas as tabuadas
+int valor1;
+int valor2;
+
+for (valor1 = 1; valor1 <= 10; valor1++) {
+    for (valor2 = 1; valor2 <= 10; valor2++) {
+        System.Console.WriteLine(valor1 * valor2 + " ");
+    }
+
+    System.Console.WriteLine();
+}
+Console.ReadKey();
